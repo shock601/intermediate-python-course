20527f45597dd69f3756cde83179ef7e1af80e8a
shock601 <c.blackwell1@yahoo.com>
2020-06-10 19:48:11 -0500

    git add dice_roller.py git commit-m "Now rolling two dice" git push git commit -m "6 rolls"

--- dice_roller.py
+++ dice_roller.py
@@ -5,9 +5,14 @@ def main():
   dice_sum = 0
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
-    dice_sum = dice_sum + roll
-    print(f'You rolled a {roll}')
-  print(f'You rolled a {dice_sum}!')
+    dice_sum += roll
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success')
+    else:
+      print(f'You rolled a {roll}')
+  print(f'You rolled a {dice_sum}')
 
 
 if __name__== "__main__":
