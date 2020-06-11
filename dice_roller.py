ca3f95a2e9e305457b646ff5318250161f597eaf
shock601 <c.blackwell1@yahoo.com>
2020-06-11 14:33:14 -0500

    git add dice_roller.py git commit -m "Tutorial complete" git push

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,15 @@
 
 def main():
   import random
-  dice_rolls = 2
+  dice_rolls = int(input('How many dice would you like to roll?'))
+  dice_size = int(input('How many sides are the dice?'))
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
     if roll == 1:
       print(f'You rolled a {roll}! Critical Fail')
-    elif roll == 6:
+    elif roll == dice_size:
       print(f'You rolled a {roll}! Critical Success')
     else:
       print(f'You rolled a {roll}')
@@ -16,4 +17,5 @@ def main():
 
 
 if __name__== "__main__":
+
   main()
